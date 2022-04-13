2454fd4203dd1949506dccddf4aaaffdcaa9eead
Stephen Laz-Eke <80836597+Chinechelum@users.noreply.github.com>
2022-04-13 15:06:09 +0100

    Delete Access-Controlled Calculator directory

--- Access-Controlled Calculator/db_connection.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("usersdetails.db")
-
-# # Adding Data to the SQLite Database
-cursor = connection.cursor()
-cursor.execute("CREATE TABLE userprofile (username TEXT, password TEXT)")
-connection.commit()
-connection.close()
